4d545a02878fa522152c5a55a0708fcb65a30bca
Ole Albers <ole@nocarrier.de>
2025-09-02 21:43:28 +0200

    added grafana

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
+using System.Reflection;
 using System.Text.Json;
 using GassiMeter;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Compact;
+using Serilog.Sinks.Grafana.Loki;
 
 internal static class Program
 {
@@ -27,10 +32,28 @@ internal static class Program
     {
         var services = new ServiceCollection();
         
-        services.AddLogging(builder =>
+        services.AddLogging(cfg => cfg.SetMinimumLevel(LogLevel.Debug));
+        services.AddSerilog(cfg =>
         {
-            builder.AddConsole();
-            builder.SetMinimumLevel(LogLevel.Information);
+            cfg.MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .Enrich.WithProperty("job", Assembly.GetEntryAssembly()?.GetName().Name)
+                .Enrich.WithProperty("service", Assembly.GetEntryAssembly()?.GetName().Name)
+                .Enrich.WithProperty("desktop", Environment.GetEnvironmentVariable("DESKTOP_SESSION"))
+                .Enrich.WithProperty("language", Environment.GetEnvironmentVariable("LANGUAGE"))
+                .Enrich.WithProperty("lc", Environment.GetEnvironmentVariable("LC_NAME"))
+                .Enrich.WithProperty("timezone", Environment.GetEnvironmentVariable("TZ"))
+                .Enrich.WithProperty("dotnetVersion", Environment.GetEnvironmentVariable("DOTNET_VERSION"))
+                .Enrich.WithProperty("inContainer",
+                    Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER"))
+                .WriteTo.GrafanaLoki(Environment.GetEnvironmentVariable("LOKIURL") ?? "http://thebeast:3100",
+                    propertiesAsLabels: ["job"]);
+            if (Assembly.GetEntryAssembly()?.GetCustomAttribute<AssemblyConfigurationAttribute>()?.Configuration ==
+                "Debug")
+            {
+                cfg.WriteTo.Console(new RenderedCompactJsonFormatter());
+            }
         });
         
         services.AddSingleton(FromJson<Config>("config.json")); 
@@ -42,6 +65,7 @@ internal static class Program
         services.AddSingleton<History>();
         
         services.AddScoped<Wled>();
+        
 
         return services.BuildServiceProvider();
     }
